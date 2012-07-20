1f02133b0da5c169ab1cd936ab2f93ed3c814f66
Mikhail Korobov <kmike84@gmail.com>
2012-07-20 20:55:46 +0600

    unicode support & benchmarks

--- src/hat_trie.pyx
+++ src/hat_trie.pyx
@@ -1,6 +1,11 @@
+# cython: profile=True
+
 cimport chat_trie
 
 cdef class BaseTrie:
+    """
+    Base HAT-Trie wrapper.
+    """
 
     cdef chat_trie.hattrie_t* _trie
 
@@ -14,9 +19,8 @@ cdef class BaseTrie:
     def __getitem__(self, bytes key):
         return self._getitem(key)
 
-    cdef int _getitem(self, bytes key) except -1:
-        cdef char* c_key = key
-        cdef chat_trie.value_t* value_ptr = chat_trie.hattrie_tryget(self._trie, c_key, len(c_key))
+    cdef int _getitem(self, char* key) except -1:
+        cdef chat_trie.value_t* value_ptr = chat_trie.hattrie_tryget(self._trie, key, len(key))
         if value_ptr == NULL:
             raise KeyError(key)
         return value_ptr[0]
@@ -24,28 +28,49 @@ cdef class BaseTrie:
     def __setitem__(self, bytes key, int value):
         self._setitem(key, value)
 
-    cdef void _setitem(self, bytes key, chat_trie.value_t value):
+    cdef void _setitem(self, char* key, chat_trie.value_t value):
         chat_trie.hattrie_get(self._trie, key, len(key))[0] = value
 
     def __contains__(self, bytes key):
         return self._contains(key)
 
-    cdef bint _contains(self, bytes key):
+    cdef bint _contains(self, char* key):
         cdef chat_trie.value_t* value_ptr = chat_trie.hattrie_tryget(self._trie, key, len(key))
         return value_ptr != NULL
 
 
 cdef class Trie(BaseTrie):
-    cdef unicode encoding
+    """
+    HAT-Trie with unicode support.
 
-    def __init__(self, encoding='latin1'):
-        self.encoding = encoding
+    XXX: Internal encoding is hardcoded as UTF8. This is the fastest
+    encoding that can handle all unicode symbols and doesn't have
+    zero bytes.
+
+    This may seem sub-optimal because it is multibyte encoding;
+    single-byte language-specific encoding (such as cp1251)
+    seems to be faster. But this is not the case because:
+
+    1) the bottleneck of this wrapper is string encoding, not trie traversal;
+    2) python's unicode encoding utilities are optimized for utf8;
+    3) users will have to select language-specific encoding for the trie;
+    4) non-hardcoded encoding causes extra overhead and prevents cython
+       optimizations.
+
+    That's why hardcoded utf8 is up to 9 times faster than configurable cp1251.
+
+    XXX: char-walking utilities may become tricky with multibyte
+    internal encoding.
+    """
 
     def __getitem__(self, unicode key):
-        return self._getitem(key.encode(self.encoding))
+        cdef bytes bkey = key.encode('utf8')
+        return self._getitem(bkey)
 
     def __contains__(self, unicode key):
-        return self._contains(key.encode(self.encoding))
+        cdef bytes bkey = key.encode('utf8')
+        return self._contains(bkey)
 
     def __setitem__(self, unicode key, int value):
-        self._setitem(key.encode(self.encoding), value)
+        cdef bytes bkey = key.encode('utf8')
+        self._setitem(bkey, value)
