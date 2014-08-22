89be37678d6e9f723da19dec2fa5adbc41ee268e
Simeon Visser <svisser@users.noreply.github.com>
2014-08-22 17:17:25 +0100

    Ensure file handles are closed in setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,19 @@ HATTRIE_DIR = 'hat-trie/src'
 HATTRIE_FILE_NAMES = ['ahtable.c', 'hat-trie.c', 'misc.c', 'murmurhash3.c']
 HATTRIE_FILES = [os.path.join(HATTRIE_DIR, name) for name in HATTRIE_FILE_NAMES]
 
+file_readme = open('README.rst')
+readme = file_readme.read()
+file_readme.close()
+
+file_changes = open('CHANGES.rst')
+changes = file_changes.read()
+file_changes.close()
+
 setup(
     name="hat-trie",
     version="0.2",
     description="HAT-Trie for Python",
-    long_description = open('README.rst').read() + "\n\n" + open('CHANGES.rst').read(),
+    long_description = readme + "\n\n" + changes,
     author='Mikhail Korobov',
     author_email='kmike84@gmail.com',
     url='https://github.com/kmike/hat-trie/',
